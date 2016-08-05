1f570df97843bb8118acd018a3cc1ecfc283688a
Jon Garrett <jonathan.garrett@gmail.com>
2016-08-05 00:07:39 -0700

    Clean up nRooks and make nQueens uglier
    Still not working. I don't understand why currentGameValues works as
    intended for rooks, but not queens. We aren't snapping back up when we
    reach a successful leaf properly...

--- src/solvers.js
+++ src/solvers.js
@@ -39,23 +39,14 @@ window.findNRooksSolution = function(n) {
 };
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var start = new Date();
   var solutions = [];
-  var possibleColumns = _.range(n);
 
   var fillBoard = function(row, possibleColumns, currentGameValues) {
-    currentGameValues = currentGameValues || [];
     if (possibleColumns.length > 0) {
-      //loop through elements in possibleColumns
       for (var i = 0; i < possibleColumns.length; i++) {
         var columnChoices = possibleColumns.slice();
-        // debugger;
-        //create current row with possible values and current row passed to function call
-        var curRow = [row, columnChoices[i]];
-        //splice out current i element from column choices
         columnChoices.splice(i, 1);
-        currentGameValues.push(curRow);
-        //pass remaining values to next recursion
+        currentGameValues.push([row, columnChoices[i]]);
         fillBoard(row + 1, columnChoices, currentGameValues);
       }
     } else {
@@ -63,10 +54,7 @@ window.countNRooksSolutions = function(n) {
       return;
     }
   };
-  fillBoard(0, possibleColumns);
-  var end = new Date();
-  //console.table (solutions);
-  console.log( end - start);
+  fillBoard(0, _.range(n), []);
   return solutions.length;
 };
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
@@ -91,112 +79,49 @@ window.findNQueensSolution = function(n) {
 };
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
+  var start = new Date();
   if (n === 0) {
     return 1;
   }
   var solutions = [];
-  var minDiags = []; 
-  var majDiags = []; 
-  var possibleColumns = _.range(n);
-
   var fillBoard = function(row, possibleColumns, minDiags, majDiags, currentGameValues) {
     if (n === 4) {
       debugger;
     }
+    var posSolution = currentGameValues.slice();
     var usedMinDiags = minDiags.slice();
     var usedMajDiags = majDiags.slice();
     if (possibleColumns.length > 0 && row < n) {
-      for (var i = 0; i < possibleColumns.length; i++) {
+      for (var i = 0; i < possibleColumns.length; i++) {        
         var columnChoices = possibleColumns.slice();
         var minDiag = columnChoices[i] - row;
         var majDiag = columnChoices[i] + row;
         //if current value does not conflict with used diagonal indexes
         if (usedMinDiags.includes(minDiag) !== true && usedMajDiags.includes(majDiag) !== true) {
           //push current value to possible solutions
-          var posSolution = currentGameValues || [];
           posSolution.push([row, columnChoices[i]]);
           //update used Diagonals
           usedMinDiags.push(minDiag);
           usedMajDiags.push(majDiag);
           //remove index from future possible columns
           columnChoices.splice(i, 1);
-          //call recursive function with updated values
         }
         //call function to recurse to next level!
         //values will be updated by previous if statement if a value was found 
         fillBoard(row + 1, columnChoices, usedMinDiags, usedMajDiags, posSolution);
       }
     } else {
-      if (Array.isArray(currentGameValues) && currentGameValues.length === n) {
-        solutions.push(currentGameValues);
+      if (Array.isArray(posSolution) && posSolution.length === n) {
+        solutions.push(posSolution);
       }
       return;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // if (n === 2) {
-    //   debugger;
-    // }
-
-    // currentGameValues = currentGameValues || [];
-    // if (currentGameValues.length === n) {
-    //   solutions.push(currentGameValues);
-    //   return;
-    // }
-    // if (row < n) {
-    //   //loop through elements in possibleColumns
-    //   for (var i = 0; i < possibleColumns.length; i++) {
-    //     var curSolutionAttempt = currentGameValues || [];
-    //     var columnChoices = possibleColumns.slice(); // Copy of the array
-    //     var usedMinDiags = minDiags.slice();
-    //     var usedMajDiags = majDiags.slice();
-    //     var min = columnChoices[i] - row;
-    //     var maj = columnChoices[i] + row; 
-
-    //     if (usedMinDiags.includes(min) !== true && usedMajDiags.includes(maj) !== true) {
-    //       currentGameValues.push([row, columnChoices[i]]);
-    //       usedMinDiags.push(min); 
-    //       usedMajDiags.push(maj); 
-    //       columnChoices.splice(i, 1);
-    //       fillBoard(row + 1, columnChoices, usedMinDiags, usedMajDiags, currentGameValues);
-    //     } else if (i === columnChoices.length - 1) { //if we have checked whole row and not found working space
-    //       //we have to pass in all possible columns as it was before we finished up this row
-    //       fillBoard(row + 1, columnChoices, usedMinDiags, usedMajDiags, currentGameValues);
-    //     }
-    //   }
-    // } else {
-    //   return;
-    // }
   };
-
-  fillBoard(0, possibleColumns, minDiags, majDiags);
-  console.table(solutions);
-  console.log('Number of solutions for ' + n + ' queens:', solutions);
+  fillBoard(0, _.range(n), [], [], []);
+  
+  var end = new Date();
+  console.log('Time to solve: ', end - start);
+  console.log('Number of solutions for ' + n + ' queens:', solutions.length);
   return solutions.length;
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
